27cb53a0300b74230d703afd5b62638a81fd1310
DealiAxy <dealiaxy@gmail.com>
2022-02-21 00:12:22 +0800

    加入多标签切换功能（虽然现在还没标签）

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,12 @@ Vue.use(vuex);
 
 // 引入子模块
 import app from './modules/app'
+import tab from './modules/tab'
 
 const store = new vuex.Store({
   modules: {
-    app: app
+    app,
+    tab
   }
 })
 

4fc77a33fc5ebeb845fbf70a51af028d2c7774ad
DealiAxy <dealiaxy@gmail.com>
2022-02-23 00:51:40 +0800

    图片相关api接入

---
+++ src/http/modules/photo.js
@@ -0,0 +1,30 @@
+import axios from '../axios'
+
+export const getList = (page = 1, pageSize = 10) => {
+  return axios({
+    url: 'Photo',
+    method: 'get',
+    params: {page, pageSize}
+  })
+}
+
+export const get = photoId => {
+  return axios({
+    url: `Photo/${photoId}/`,
+    method: 'get',
+  })
+}
+
+// 上传图片
+export const add = (title, file) => {
+  let formData = new FormData()
+  formData.append('title', title)
+  formData.append('file', file)
+
+  return axios({
+    url: 'Photo',
+    method: 'post',
+    headers: {'Content-Type': 'multipart/form-data;charset=UTF-8'},
+    data: formData
+  })
+}

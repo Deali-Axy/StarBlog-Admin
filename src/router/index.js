253377319c05ac4e5de45e775ba2e278391d7953
DealiAxy <dealiaxy@gmail.com>
2022-02-23 00:51:16 +0800

    增加图片管理页面

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import NotFound from '@/views/404'
 import Intro from "@/views/Intro/Intro"
 import Test from "@/views/Intro/Test"
 import Posts from "@/views/Blog/Posts"
+import Photos from "@/views/Photography/Photos"
 
 Vue.use(Router)
 
@@ -27,21 +28,30 @@ export default new Router({
           }
         },
         {
+          path: 'posts',
+          name: '文章列表',
+          component: Posts,
+          meta: {
+            icon: 'fa fa-list fa-lg',
+            index: 1
+          }
+        },
+        {
+          path: 'photos',
+          name: '图片列表',
+          component: Photos,
+          meta: {
+            icon: 'fa fa-picture-o fa-lg',
+            index: 1
+          }
+        },
+        {
           path: 'test',
           name: 'test',
           component: Test,
           meta: {
             icon: 'fa fa-home fa-lg',
-            index: 1
-          }
-        },
-        {
-          path: 'posts',
-          name: 'posts',
-          component: Posts,
-          meta: {
-            icon: 'fa fa-home fa-lg',
-            index: 1
+            index: 3
           }
         },
       ]

7987f1f3529f47f765ae2a80fd2b59c61a97d4bd
DealiAxy <dealiaxy@gmail.com>
2022-02-24 23:53:06 +0800

    加入了导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ import Photos from "@/views/Photography/Photos"
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
@@ -68,3 +68,23 @@ export default new Router({
     },
   ]
 })
+
+
+router.beforeEach((to, from, next) => {
+  // 登录界面登录成功之后，会把用户信息保存在会话
+  // 存在时间为会话生命周期，页面关闭即失效。
+  let userName = sessionStorage.getItem('user')
+  if (to.path === '/login') {
+    // 如果是访问登录界面，如果用户会话信息存在，代表已登录过，跳转到主页
+    if (userName) next({path: '/'})
+    else next()
+  } else {
+    if (!userName) {
+      // 如果访问非登录界面，且户会话信息不存在，代表未登录，则跳转到登录界面
+      next({path: '/login'})
+    }
+    next()
+  }
+})
+
+export default router

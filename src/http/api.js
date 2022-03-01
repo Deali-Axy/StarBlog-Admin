9a2a58762e7dd1e6c8aa48bd0e53a9641f41fd18
DealiAxy <dealiaxy@gmail.com>
2022-03-02 00:07:00 +0800

    完善API封装

--- src/http/api.js
+++ src/http/api.js
@@ -3,8 +3,19 @@ import * as auth from './modules/auth'
 import * as blog from './modules/blog'
 import * as blogPost from './modules/blogPost'
 import * as category from './modules/category'
+import * as featuredCategory from './modules/featuredCategory'
+import * as featuredPhoto from './modules/featuredPhoto'
+import * as featuredPost from './modules/featuredPost'
 import * as photo from './modules/photo'
-import * as user from './modules/user'
 
 // 默认全部导出
-export default {auth, blog, blogPost, category, photo, user}
+export default {
+  auth,
+  blog,
+  blogPost,
+  category,
+  featuredCategory,
+  featuredPhoto,
+  featuredPost,
+  photo
+}

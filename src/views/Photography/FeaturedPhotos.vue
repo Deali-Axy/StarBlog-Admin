3870d250dc3693ce7e788008b8421b3a88f6656b
DealiAxy <dealiaxy@gmail.com>
2022-03-02 22:14:37 +0800

    完成推荐图片管理

--- src/views/Photography/FeaturedPhotos.vue
+++ src/views/Photography/FeaturedPhotos.vue
@@ -1,10 +1,85 @@
 <template>
-  <el-empty description="推荐图片"></el-empty>
+  <el-container>
+    <el-header height="30px">
+      <el-row type="flex" justify="start">
+        <div>
+          <el-button @click="">添加</el-button>
+        </div>
+      </el-row>
+    </el-header>
+    <el-main>
+      <el-row v-if="featuredPhotos.length>0" :gutter="10">
+        <el-col :span="4" v-for="fp in featuredPhotos" :key="fp.id">
+          <el-card :body-style="{ padding: '0px' }">
+            <el-image :src="fp.photo.url" class="image" alt="" :preview-src-list="[fp.photo.url]"></el-image>
+            <div style="padding: 14px;">
+              <div>图片标题：{{ fp.photo.title }}</div>
+              <div>拍摄地点：{{ fp.photo.location }}</div>
+              <div style="margin-top: 3px;">
+                <time class="time">{{ fp.photo.dateTimeStr }}</time>
+              </div>
+              <div class="bottom clearfix" style="margin-top: 3px;">
+                <el-button-group>
+                  <el-button type="info" icon="el-icon-warning-outline" plain @click="notImpl"></el-button>
+                  <el-button type="primary" icon="el-icon-edit" plain @click="notImpl"></el-button>
+                  <el-button type="danger" icon="el-icon-delete" plain @click="deleteItem(fp)"></el-button>
+                </el-button-group>
+              </div>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+      <el-empty v-else description="推荐图片"></el-empty>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
+import {baseUrl} from "@/utils/global"
+
 export default {
-  name: "FeaturedPhotos"
+  name: "FeaturedPhotos",
+  components: {},
+  data() {
+    return {
+      featuredPhotos: [],
+    }
+  },
+  mounted() {
+    this.loadData()
+  },
+  methods: {
+    loadData() {
+      this.$api.featuredPhoto.getAll().then(res => {
+        console.log(res)
+        this.featuredPhotos = res.data
+        this.featuredPhotos.forEach(item => {
+          item.photo.url = `${baseUrl}/media/photography/${item.photo.id}.jpg`
+          let dt = new Date(item.photo.createTime)
+          item.photo.dateTimeStr = `${dt.toLocaleDateString()} ${dt.toLocaleTimeString()}`
+        })
+        console.log('featuredPhotos', this.featuredPhotos)
+      })
+    },
+    notImpl() {
+      this.$message('还没实现哦~')
+    },
+    deleteItem(featuredPhoto) {
+      this.$confirm('此操作将删除这个推荐图片, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$api.featuredPhoto.deleteItem(featuredPhoto.id)
+          .then(res => {
+            this.$message.success(`删除成功。${res.message}`)
+            // 删除完成，重新加载数据
+            this.loadData()
+          })
+          .catch(res => this.$message.error(`操作失败。${res.message}`))
+      }).catch(() => this.$message('已取消删除'))
+    }
+  }
 }
 </script>
 

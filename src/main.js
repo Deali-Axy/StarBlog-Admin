b392dbf86b211541c1ccf6cb5deb63141f0a4fe8
DealiAxy <dealiaxy@gmail.com>
2022-03-04 16:25:47 +0800

    新增全局异常处理

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import api from './http'
-import global from '@/utils/global'
+import Exceptionless from './utils/exceptionless'
+import ErrorHandler from "./utils/errorHandler"
+import global from './utils/global'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -16,6 +18,10 @@ Vue.config.productionTip = false
 Vue.use(ElementUI)
 // 引入API模块
 Vue.use(api)
+// Exceptionless模块
+Vue.use(Exceptionless)
+// 全局异步处理模块
+Vue.use(ErrorHandler)
 
 // 挂载全局配置模块
 Vue.prototype.global = global
---
+++ src/utils/errorHandler.js
@@ -0,0 +1,68 @@
+import {ExceptionlessClient} from 'exceptionless/dist/exceptionless';
+
+const exLessClient = ExceptionlessClient.default;
+
+function isPromise(ret) {
+  return (ret && typeof ret.then === 'function' && typeof ret.catch === "function")
+}
+
+const errorHandler = (error, vm, info) => {
+  console.error('抛出全局异常', 'vm=', vm, 'info=', info)
+  console.error(error)
+  exLessClient.submitException(error)
+}
+
+function registerActionHandle(actions) {
+  Object.keys(actions).forEach(key => {
+    let fn = actions[key]
+    actions[key] = function (...args) {
+      let ret = fn.apply(this, args)
+      if (isPromise(ret)) {
+        return ret.catch(errorHandler)
+      } else { // 默认错误处理
+        return ret
+      }
+    }
+  })
+}
+
+const registerVuex = (instance) => {
+  if (instance.$options['store']) {
+    let actions = instance.$options['store']['_actions'] || {}
+    if (actions) {
+      let tempActions = {}
+      Object.keys(actions).forEach(key => {
+        tempActions[key] = actions[key][0]
+      })
+      registerActionHandle(tempActions)
+    }
+  }
+}
+const registerVue = (instance) => {
+  if (instance.$options.methods) {
+    let actions = instance.$options.methods || {}
+    if (actions) {
+      registerActionHandle(actions)
+    }
+  }
+}
+
+let GlobalError = {
+  install: (Vue, options) => {
+    /**
+     * 全局异常处理
+     * @param {*} error
+     * @param {*} vm
+     */
+    Vue.config.errorHandler = errorHandler
+    Vue.mixin({
+      beforeCreate() {
+        registerVue(this)
+        registerVuex(this)
+      }
+    })
+    Vue.prototype.$throw = errorHandler
+  }
+}
+
+export default GlobalError
